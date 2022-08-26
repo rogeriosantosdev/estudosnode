d31087f08be7d46c3d543cdaca23d23b364b501a
Rogerio Santos <rogeriosantosdev@gmail.com>
2022-08-26 14:01:59 -0300

    mongoose and api complete module

---
+++ 12_API/post_route/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+//routes - endpoints
+
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(name)
+    console.log(price)
+
+    res.json({message: `O produto ${name} foi criado com sucesso`})
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Primeira rota criada com sucesso!'})
+})
+
+
+app.listen(3000)
---
+++ 12_API/status_response/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+//routes - endpoints
+
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(name)
+    console.log(price)
+
+    if(!name){
+        res.status(422).json({message: 'O campo nome é obrigatório!'})
+    }
+
+    res.status(201).json({message: `O produto ${name} foi criado com sucesso`})
+})
+
+app.get('/', (req, res) => {
+    res.json({message: 'Primeira rota criada com sucesso!'})
+})
+
+
+app.listen(3000)
